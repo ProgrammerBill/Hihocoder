b1703d314f1d64c75b771364e982435d40e4c38e
BillChan <cjcbill@gmail.com>
2015-09-29 11:00:28 +0800

    unfinished

--- Highway/code.cpp
+++ Highway/code.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 struct Car{
    float start;
@@ -22,18 +23,39 @@ int main(){
  vector<float>time(N,0.0);
  time[0] = team[0].end - team[0].start / team[0].MaxSpeed;
  for(int i = 1; i < N;i++){
-    for(int j = i + 1;j < N;j++){ 
-     float t = (team[i].start - team[j].start) / (team[i].MaxSpeed - team[j].MaxSpeed);
-     float send = team[i].start + t * team[i].MaxSpeed;
-     if(send >= team[i].end){
+   
+   float cur = team[i].start;
+   //遍历在之前的所有车 
+   for(int j = i - 1;j >= 0 && cur < team[j].end;j--){
+     //计算第i辆车以最大速度行驶时,相遇点是否在第j辆车到达终点之前 
+     float t =abs( (team[i].start - team[j].start) / (team[i].MaxSpeed - team[j].MaxSpeed));
+     float send = team[j].start + t * team[j].MaxSpeed;
+     //第i辆车可以以最大速度开往终点
+     if(send >= team[j].end){
          time[i] = (team[i].end - team[i].start)/team[i].MaxSpeed;
+         cur = team[i].end;
+         break;
      }
      else{
+       if(team[i].end >= team[j].end){       
+         time[i] = time[j];
+         cur = team[j].end;     
+       }
+       else{
          
+       }
      }
-
    }
+
+   if(team[i].end - cur){
+       time[i] += (team[i].end -cur) / team[i].MaxSpeed;
+   }
+  
  }
+
+   for(int k = 0; k < N;k++){
+     cout<<fixed<<setprecision(2)<<time[k]<<endl;
+   }
 
  return 0;
 }
